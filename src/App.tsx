e9d8a682f484729ca68e176e0ea134d5094b3794
Andres Cardoso <andr33sdev@gmail.com>
2024-06-01 13:07:09 -0300

    typescript adjustements

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Currency } from "./types";
 
 const App = () => {
-  const [accessToken, setAccessToken] = useState(null);
   const [currencies, setCurrencies] = useState([]);
-
-  const fetchAccessToken = async () => {
-    try {
-      const response = await axios.get("https://backend-expedicion-ml.vercel.app/refresh-token");
-      setAccessToken(response.data.access_token);
-    } catch (error) {
-      console.error("Error al obtener el access token:", error);
-    }
-  };
 
   const fetchCurrencies = async () => {
     try {
-      const response = await axios.get("https://backend-expedicion-ml.vercel.app/currencies");
+      const response = await axios.get(
+        "https://backend-expedicion-ml.vercel.app/currencies"
+      );
       setCurrencies(response.data);
     } catch (error) {
       console.error("Error al obtener las monedas:", error);
@@ -24,15 +17,14 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchAccessToken();
     fetchCurrencies();
-  }, []);
+  }, [currencies]);
 
   return (
     <div>
       <h1>Currencies</h1>
       <ul>
-        {currencies.map((currency) => (
+        {currencies.map((currency: Currency) => (
           <li key={currency.id}>
             <strong>{currency.id}</strong>: {currency.description}
           </li>
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export type Currency = {
+  id: "string";
+  description: "string";
+};
