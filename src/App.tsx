a94b0ff37fb6ea005f8b896840bbae9993a57782
Andres Cardoso <andr33sdev@gmail.com>
2024-06-01 12:38:09 -0300

    sync with backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,53 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import './App.css'
+const App = () => {
+  const [accessToken, setAccessToken] = useState(null);
+  const [currencies, setCurrencies] = useState([]);
 
-function App() {
+  const fetchAccessToken = async () => {
+    try {
+      const response = await axios.get("https://backend-expedicion-ml.vercel.app/refresh-token");
+      setAccessToken(response.data.access_token);
+    } catch (error) {
+      console.error("Error al obtener el access token:", error);
+    }
+  };
+
+  const fetchCurrencies = async () => {
+    try {
+      const response = await axios.get("https://backend-expedicion-ml.vercel.app/currencies");
+      setCurrencies(response.data);
+    } catch (error) {
+      console.error("Error al obtener las monedas:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAccessToken();
+    fetchCurrencies();
+  }, []);
+
+  useEffect(() => {
+    if (accessToken) {
+      console.log(accessToken)
+    } else {
+      console.log('Solicitando access token')
+    }
+  }, [accessToken])
 
   return (
-    <>
-      <h1>Prueba</h1>
-    </>
-  )
-}
+    <div>
+      <h1>Currencies</h1>
+      <ul>
+        {currencies.map((currency) => (
+          <li key={currency.id}>
+            <strong>{currency.id}</strong>: {currency.description}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
